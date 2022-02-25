0366c5cc2deadab7046dec5ff60bc0d14e4f6d49
Theo <>
2022-02-25 11:01:34 -0500

    fix: startup script

--- start.sh
+++ start.sh
@@ -3,11 +3,14 @@
 echo 'For any help, please open an issue on https://github.com/inferrd/inferrd'
 
 ip=""
+nomad_ip=""
 
 if [ "$(uname)" == "Darwin" ]; then
     ip='host.docker.internal'
+    nomad_ip='0.0.0.0'
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     ip='172.17.0.1'
+    nomad_ip=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo 'Windows is not supported. Only Linux and Mac OS are supported platforms.'
     exit
@@ -23,5 +26,6 @@ then
 fi
 
 export MACHINE_NETWORK_ROUTE=$ip
+export NOMAD_IP=$nomad_ip
 
 docker-compose up $@
